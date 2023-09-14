0e140d2c9a19c5ce0e3a82a333096460aab89d22
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2023-09-14 20:19:07 +0530

    Time: 1159 ms (42.94%) | Memory: 7.3 MB (7.08%) - LeetSync

--- 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
 
    int ans;
-   void backtrack(int i,vector<int> & cookies, vector<int> & v, int sum){
+   void backtrack(int i,vector<int> & cookies, vector<int> & v){
        if(i==cookies.size()){
-           ans=min(sum,ans);
+           ans=min(*max_element(v.begin(),v.end()),ans);
            return ;
        }
 
        for(int k=0;k<v.size();k++){
            v[k]+=cookies[i];
-           backtrack(i+1,cookies,v,max(v[k],sum));
+           backtrack(i+1,cookies,v);
             v[k]-=cookies[i];
        }
    }
     int distributeCookies(vector<int>& cookies, int k) {
         ans = INT_MAX;
         vector<int> buckets(k, 0);
-        backtrack(0, cookies, buckets, 0);
+        backtrack(0, cookies, buckets);
         return ans;
     }
 };
