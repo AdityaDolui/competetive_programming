7a3556ac037eb8c8792ae971dcbc51f3825b635b
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2023-12-08 20:07:15 +0530

    Time: 396 ms (80.80%) | Memory: 16.7 MB (91.08%) - LeetSync

--- 3223-count-complete-substrings/count-complete-substrings.cpp
+++ 3223-count-complete-substrings/count-complete-substrings.cpp
@@ -4,15 +4,15 @@ public:
         int ans=0;
         int len,cnt;
         int n=word.size();
-        
+         vector<int> freq(26,0);
         for(int i=1;i<=26;i++){
              len=i*k;
             cnt=0;
             if(n<len)break;
-           // int l=0,h=0;
-            //fill(freq.begin(),freq.end(),0);
-            vector<int> freq(26,0);
-            for(int l=0,h=0;h<n;h++){
+            int l=0,h=0;
+            fill(freq.begin(),freq.end(),0);
+           
+            while(h<n){
                 
                 if((h>0) && abs(word[h]-word[h-1])>2){
                     cnt=0;
@@ -33,6 +33,7 @@ public:
             //h++;
             }
             if(((h-l+1)==len) && (cnt==i))ans++;
+            h++;
         }
         }
         return ans;
