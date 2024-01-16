c49f4d2b8f757313279d446c1e34e97e2037d5f4
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-01-16 19:02:02 +0530

    Time: 3 ms (83.40%) | Memory: 6.6 MB (31.60%) - LeetSync

---
+++ 3240-maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k/maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k.cpp
@@ -0,0 +1,45 @@
+#define ll long long
+class Solution {
+public:
+     int countBit(ll val){
+         int ans=0;
+         while(val){
+             val=val>>1;
+             ans++;
+         }
+         return ans;
+     }
+     ll powerOf2(int i){
+         return 1LL << i;
+     }
+    ll value(ll val, int x){
+
+        int cnt=countBit(val);
+        val++;
+        ll ans=0;
+        int i=1;
+        while(i<=cnt){
+            if((i%x)==0){
+                ans+=(val/powerOf2(i)*powerOf2(i-1))+ max(0LL,(val%powerOf2(i))-powerOf2(i-1));
+            }
+            i++;
+        }
+        return ans;
+
+    }
+    long long findMaximumNumber(long long k, int x) {
+        
+        ll low=0, high =1e15,mid;
+        ll ans;
+
+        while(low<=high){
+            mid=(high - low) / 2 + low;
+            ll cnt=value(mid,x);
+            if(cnt<=k){
+                ans=cnt;
+                low=mid+1;
+            }else high=mid-1;
+        }
+        return high;
+    }
+};
