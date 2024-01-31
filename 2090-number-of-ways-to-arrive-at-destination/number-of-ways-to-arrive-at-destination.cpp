bae595bbb03f38345cfbf543d343ff4155faa036
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-02-01 04:53:45 +0530

    Time: 50 ms (96.13%) | Memory: 33.9 MB (43.97%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -9,7 +9,7 @@ int mod = 1e9 + 7;
             adj[roads[i][0]].push_back({roads[i][1], roads[i][2]});
             adj[roads[i][1]].push_back({roads[i][0], roads[i][2]});
         }
-       // vector<int> vis(n,0);
+        vector<int> vis(n,0);
         vector<long long> dist(n,1e15);
         vector<int> cnt(n,0);
         priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<pair<long long,int>>> pq;
@@ -20,8 +20,8 @@ int mod = 1e9 + 7;
             long long w=pq.top().first;
             int curr=pq.top().second;
             pq.pop();
-          //  if(vis[curr])continue;
-          //  vis[curr]=1;
+            if(vis[curr])continue;
+            vis[curr]=1;
             if(curr==n-1)break;
 
             for(auto it: adj[curr]){
