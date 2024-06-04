3c5bd764ee285642728d7d69992bc076c1bdef75
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-06-04 16:45:20 +0530

    Time: 190 ms (100.00%) | Memory: 116.8 MB (49.54%) - LeetSync

---
+++ 1832-minimum-operations-to-make-a-subsequence/minimum-operations-to-make-a-subsequence.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+int count (vector<int>& arr){
+    vector<int> ans;
+    
+    for(auto it:arr){
+        if(ans.empty() || it>ans.back()){
+            ans.push_back(it);
+        }
+        else{
+            auto i=lower_bound(ans.begin(),ans.end(),it);
+            if(i==ans.end())i--;
+            int ind=i-ans.begin();
+            ans[ind]=it;
+        }
+    }
+    return ans.size();
+}
+    int minOperations(vector<int>& target, vector<int>& arr) {
+        unordered_map<int,int> map;
+        int n=target.size();
+        int m=arr.size();
+        for(int i=0;i<n;i++){
+            map[target[i]]=i;
+        }
+
+        vector<int> temp;
+        for(auto it:arr){
+            if(map.find(it)!=map.end())temp.push_back(map[it]);
+        }
+        
+    int len=count(temp);
+    return n-len;
+    }
+};
