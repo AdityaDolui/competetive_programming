c4e5bb50f8e8d4dd960834c1ece0d148ac320bc3
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-06-06 08:08:30 +0530

    Time: 49 ms (84.44%) | Memory: 30.4 MB (78.12%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -5,11 +5,8 @@ public:
         unordered_map<int,int> map;
         sort(hand.begin(),hand.end());
         for(auto it:hand)map[it]++;
-
         for(auto it:hand){
-            if(map[it]>0){
-                
-             
+            if(map[it]>0){          
                 for(int i=it;i<it+groupSize;i++){
                     if(map[i]<=0)return 0;
                     else map[i]--;
