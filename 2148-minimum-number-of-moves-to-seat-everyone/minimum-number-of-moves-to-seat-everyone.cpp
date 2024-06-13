3f7ae1c1cff306212e3a02cc8a166c8a2218b5c8
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-06-13 17:52:52 +0530

    Time: 9 ms (37.04%) | Memory: 21 MB (52.06%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
@@ -4,8 +4,8 @@ public:
         int ans=0;
         sort(seats.begin(),seats.end());
         sort(students.begin(),students.end());
-
-        for(int i=0;i<seats.size();i++){
+        int n=seats.size();
+        for(int i=0;i<n;i++){
             ans+=abs(seats[i]-students[i]);
         }
         return ans;
