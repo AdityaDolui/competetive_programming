95e2a233d9df59a70353f1972e22dcde106af57e
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-06-19 05:09:47 +0530

    Time: 1677 ms (5.87%) | Memory: 37.6 MB (98.29%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        // n^m time complexity  1 2 3 4 4 5 , 4
+        // n^m time complexity  
         int ans=0;
         int m=difficulty.size();
         for(auto it:worker){
             int maxi=0;
-            int point=-1;
             for(int i=0;i<m;i++){
                 if(difficulty[i]<=it){
                     maxi=max(maxi,profit[i]);
