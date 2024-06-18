a4b25865aec434aee5eb879007228db14e5da57b
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-06-19 04:42:19 +0530

    Time: 52 ms (77.32%) | Memory: 53.4 MB (50.81%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -3,14 +3,34 @@ public:
     int firstMissingPositive(vector<int>& nums) {
         int i=0;
         int n=nums.size();
-        while(i<n){
-            if(nums[i]>0 && nums[i]<=n && nums[i]!=nums[nums[i]-1]){
-                swap(nums[i],nums[nums[i]-1]);
-            }else i++;
+        bool one =false;
+        for(auto &it:nums){
+            if(it==1)one=true;
+            if(it<=0)it=1;
+            
         }
-        for(int i=0;i<n;i++){
-            if(i+1!=nums[i])return i+1;
+        if(!one)return 1;
+
+    for(int i=0;i<n;i++){
+        if(nums[i]<=n){
+            int val=abs(nums[i])-1;
+        nums[val]=-abs(nums[val]);
         }
-        return n+1;
+    }
+    //for(auto it:nums)cout<<it<<" ";
+    for(int i=0;i<n;i++){
+        if(nums[i]>0)return i+1;
+    }
+    return n+1;
+
+        // while(i<n){
+        //     if(nums[i]>0 && nums[i]<=n && nums[i]!=nums[nums[i]-1]){
+        //         swap(nums[i],nums[nums[i]-1]);
+        //     }else i++;
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(i+1!=nums[i])return i+1;
+        // }
+        // return n+1;
     }
 };
