f67c950575a3629d28460d11a3f337e502c49f6c
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-06-19 02:06:30 +0530

    Time: 34 ms (6.11%) | Memory: 27.7 MB (6.89%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+     unordered_set<int> set;
+     int mini=INT_MAX;
+     int maxi=0;
+     for(auto it:nums){
+        set.insert(it);
+        mini=min(mini,it);
+        maxi=max(maxi,it);
+     }   
+     int ans=0;
+     for(int i=0;i<=maxi;i++){
+        if(set.find(i)==set.end())return i;
+     }
+     return maxi+1;
+    }
+};
