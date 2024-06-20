f40f76a357e73ff74a9fa17f74ea5c8c22270f3c
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-06-21 02:23:44 +0530

    Time: 133 ms (30.31%) | Memory: 61.4 MB (73.95%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -3,22 +3,20 @@ public:
 bool check(vector<int>& position, int m,int ans){
     int prev=position[0];
     m--;
-    int i=1;
-    while(i<position.size()){
-        int it=lower_bound(position.begin()+i,position.end(),prev+ans)-position.begin();
-        if(it!=position.size()){
-            prev=position[it];
-            m--;
-            if(!m)return true;
-        }else break;
+    for(int i=1;i<position.size();i++){
+     if(position[i]-prev>=ans){
+        m--;
+        prev=position[i];
+}
+if(!m)return true;
     }
 return false;
 }
     int maxDistance(vector<int>& position, int m) {
             sort(position.begin(),position.end());
-       // int mini=*min_element(position.begin(),position.end());
-       //   int maxi=*max_element(position.begin(),position.end());
-          int h=1e9;
+        //int mini=*min_element(position.begin(),position.end());
+         // int maxi=*max_element(position.begin(),position.end());
+          int h= 1e9; //(maxi-mini);
           int l=0;
           int ans=0;
           while(l<=h){
