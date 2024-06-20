d1db2fc5da0f18f2d1d3b200d99f589d2b4dd1f7
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-06-21 02:24:04 +0530

    Time: 134 ms (26.88%) | Memory: 61.4 MB (73.95%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -14,9 +14,9 @@ return false;
 }
     int maxDistance(vector<int>& position, int m) {
             sort(position.begin(),position.end());
-        //int mini=*min_element(position.begin(),position.end());
-         // int maxi=*max_element(position.begin(),position.end());
-          int h= 1e9; //(maxi-mini);
+        int mini=*min_element(position.begin(),position.end());
+          int maxi=*max_element(position.begin(),position.end());
+          int h= (maxi-mini);
           int l=0;
           int ans=0;
           while(l<=h){
