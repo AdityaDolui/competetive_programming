29cfcb912e2f8f2b5e2d1f49be402d16eacb0ac4
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-07-12 18:28:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (59.86%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -8,21 +8,21 @@ public:
             bool flag=0;
             size_t found=s.find(")");
             if(found==string::npos)return s;
-           // cout<<found<<" ";
+          
             string temp="";
             s.erase(found,1);
             int i=found-1;
             for(;s[i]!='(';i--){
                 temp+=s[i];
             }
-            //cout<<i<<" |"<<temp<<"| ";
+           
             s.erase(i,1);
             int k=0;
             while(i<found-1){
                 s[i++]=temp[k++];
-             //   cout<<"*";
+             
             }
-            //cout<<s;
+           
         }
         return s;
     }
