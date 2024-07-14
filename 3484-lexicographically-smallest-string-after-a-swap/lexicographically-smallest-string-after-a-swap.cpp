600793adc38cea9e15275aa0523ecb2f0a39fbc4
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-07-14 12:14:44 +0530

    Time: 5 ms (66.67%) | Memory: 8.1 MB (83.33%) - LeetSync

--- 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.cpp
+++ 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.cpp
@@ -6,7 +6,7 @@ public:
             int num1=(int)s[i];
             int num2=(int)s[i+1];
             cout<<num1<<num2;
-            if((num1&1 && num2&1) || (num1%2==0 && num2%2==0)){
+            if((num1&1 && num2&1) || (!(num1&1) && !(num2&1))){
                 cout<<"here";
             if(num1>num2){
                 cout<<"yes";
