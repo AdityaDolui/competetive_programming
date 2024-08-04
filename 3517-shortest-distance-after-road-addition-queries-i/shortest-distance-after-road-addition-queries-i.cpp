5f2fc7f3a795b588c7c4fdcd067e7c4f892fe5c6
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-08-05 01:06:32 +0530

    Time: 348 ms (14.29%) | Memory: 129.2 MB (14.29%) - LeetSync

--- 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.cpp
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.cpp
@@ -33,29 +33,5 @@ public:
         }
         return ans;
         
-        
-        
-        
-        
-        
-        // vector<pair<int,int>> pair;
-        // int gap=0;
-        // for(int i=0;i<queries.size();i++){
-        //     auto itt=queries[i];
-        //     int l=itt[0];
-        //     int r=itt[1];
-        //     bool flag=true;
-        //     for(auto &it:pair){
-        //         if((l<=it.first &&  r>=it.first)||(l>=it.first &&  l<=it.second)){
-        //             if(l<=it.first &&  r>=it.first){
-        //                 gap+=abs(l-it.first);
-        //                 it.first=l;
-        //             }
-        //             else{
-        //                 gap++abs(it.second-r)
-        //             }
-        //         }
-        //     }
-        // }
     }
 };
