fde17bfabc421a15dd2481bc1faac0040dc8f9da
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-08-22 17:36:08 +0530

    Time: 59 ms (22.00%) | Memory: 49.3 MB (9.00%) - LeetSync

--- 476-number-complement/number-complement.js
+++ 476-number-complement/number-complement.js
@@ -6,8 +6,8 @@ var findComplement = function(num) {
     let s=num.toString(2);
     let str="";
     //console.log(s);
-    for(let char of s){
-    str+=char==='1'?'0':'1';
+    for(let ch in s){
+    str+=s[ch]==='1'?'0':'1';
     }
    // console.log(str)
     return parseInt(str,2);
