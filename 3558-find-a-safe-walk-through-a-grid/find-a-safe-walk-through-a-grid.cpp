b09ae3070c7b385a95ce4d24d757c3f1e0e0c79b
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-09-15 00:57:27 +0530

    Time: 109 ms (33.33%) | Memory: 56.7 MB (22.22%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -4,41 +4,52 @@ public:
     int dy[4]={0,0,1,-1};
    
     
-    bool f(int i,int j,vector<vector<int>> &grid, int health, vector< vector<vector<int>>>& dp){
-         if(grid[i][j]==1){
+    bool f(int i,int j,vector<vector<int>> &grid,vector<vector<bool>>& vis, int health, vector< vector<vector<int>>>& dp){
+          if(i<0 || i>=grid.size()|| j<0 || j>=grid[0].size()||vis[i][j]||health<=0)return false;
+        
+           if(grid[i][j]==1){
             if(health<=1)return false;
             health=health-1;
         }
+        
         if(i==grid.size()-1 && j==grid[0].size()-1){
             return true;
         }
         
+        
+      
+        
         if(dp[i][j][health]!=-1)return dp[i][j][health];
        
-        int curr=grid[i][j];
+        
+     
+        
+        vis[i][j]=true;
+        
+        
+        
         for(int k=0;k<=3;k++){
             int x=dx[k]+i;
             int y=dy[k]+j;
-           grid[i][j]=-1;
-           if(x>=0 && x<grid.size()&& y>=0 && y<grid[0].size() && grid[x][y]!=-1){
-            if(grid[x][y] && health<=1)continue;
-           bool ans=f(x,y,grid,health,dp);
-            if(ans)return dp[i][j][health]= true;
-           }
            
+           
+           bool ans=f(x,y,grid,vis,health,dp);
+            if(ans)return dp[i][j][health]= true;
+             
             
         }
-          grid[i][j]=curr;
-       
+        vis[i][j]=0;
         return dp[i][j][health]= false;
         
     }
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
         int m=grid[0].size();
         int n=grid.size();
+        vector<vector<bool>> vis (n,vector<bool>(m,0));
+       // vector<vector<int>> dp(n,vector<int>(m,-1));
         vector<vector<vector<int>>> dp (n, vector<vector<int>>(m,vector<int>(health+1,-1)));
-        return f(0,0,grid,health,dp);
+        bool ans= f(0,0,grid,vis,health,dp);
        
-
+        return ans;
     }
 };
