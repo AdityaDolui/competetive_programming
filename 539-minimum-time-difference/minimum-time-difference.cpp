708bd8123e346403156c0404fec4aa40f91539ca
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-09-17 05:43:53 +0530

    Time: 23 ms (6.56%) | Memory: 18 MB (30.13%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.cpp
+++ 539-minimum-time-difference/minimum-time-difference.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    int findMinDifference(vector<string>& tp) {
-    int mini = INT_MAX;
+    int findMinDifference(vector<string>& timePoints) {
+    vector<int> arr,arr2;
+    for(auto it:timePoints){
+        int val=(stoi(it.substr(0,2)))*60;
+        val+=stoi(it.substr(3,2));
+        arr.push_back(val);
+        arr.push_back((val-(24*60)));
 
-        vector<int> minutes;
-        for(auto& val:tp){ // int i=0;i<v.size();i++
-
-            int hr = 10 * (val[0] - '0') + (val[1] - '0'); // 2*10 + 3 = 23
-            // val[2] = :
-            int min = 10 * (val[3] - '0') + (val[4] - '0'); // 5*10 + 9 = 59
-
-            // convert whole to minute starting point -> 00            
-            minutes.push_back(hr * 60 + min);
-        }
-
-        // Sort the minutes
-        sort(minutes.begin(), minutes.end());
-
-        // Calculate the minimum difference between adjacent times
-        for(int i = 1; i < minutes.size(); i++){
-            mini = min(mini, minutes[i] - minutes[i-1]);
-        }
-
-        // Also, check the difference between the first and last time, considering the 24-hour wraparound
-        int n = minutes.size();
-        mini = min(mini, 1440 - (minutes[n-1] - minutes[0]));
-
-        return mini;
+    }
+    sort(arr.begin(),arr.end());
+    int ans=INT_MAX;
+    for(int i=1;i<arr.size();i++)ans=min(ans,(arr[i]-arr[i-1]));
+        for(auto it:arr)cout<<it<<" ";
+    return ans;
     }
 };
