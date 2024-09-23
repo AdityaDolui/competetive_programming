1b2082e550e69431158326a198152bb177070166
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-09-24 05:11:33 +0530

    Time: 413 ms (12.44%) | Memory: 94.4 MB (66.13%) - LeetSync

---
+++ 2329-maximum-product-after-k-increments/maximum-product-after-k-increments.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+int mod=1e9+7;
+    int maximumProduct(vector<int>& nums, int k) {
+        
+        priority_queue<int,vector<int>,greater<int>> pq;
+        for(auto it:nums)pq.push(it);
+
+        while(k--){
+            int val=pq.top();
+            pq.pop();
+            val++;
+            pq.push(val);
+        }
+        long long ans=1;
+        while(!pq.empty()){
+            ans=((pq.top()%mod)*ans)%mod;
+            pq.pop();
+        }
+        return ans;
+    }
+};
