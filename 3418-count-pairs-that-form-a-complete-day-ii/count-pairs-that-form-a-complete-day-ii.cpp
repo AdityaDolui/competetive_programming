fae12a0f9629bc7a29d5b9d79d7729de14ab6ebc
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-10-02 04:37:16 +0530

    Time: 162 ms (42.14%) | Memory: 139.9 MB (48.96%) - LeetSync

--- 3418-count-pairs-that-form-a-complete-day-ii/count-pairs-that-form-a-complete-day-ii.cpp
+++ 3418-count-pairs-that-form-a-complete-day-ii/count-pairs-that-form-a-complete-day-ii.cpp
@@ -5,21 +5,16 @@ public:
 
         long long ans=0;
         for(auto it:hours){
+
             if(!(it%24)){
                 ans+=map[0];
                 map[0]++;
             }else{
-
-        
-            if(map[24-(it%24)]>0){
-                cout<<it<<" ";
-                ans+=map[24-(it%24)];}
+            ans+=map[24-(it%24)];
             map[(it%24)]++;
         }
         }
-        for(auto it:map){
-            cout<<it.first<<"-"<<it.second<<" || ";
-        }
+    
         return ans;
     }
 };
