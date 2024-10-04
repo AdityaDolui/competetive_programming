795ce7d7d88d588474a3555f875c4c5cb0c70e3a
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-10-04 18:43:31 +0530

    Time: 122 ms (93.01%) | Memory: 70.4 MB (85.49%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -1,36 +1,29 @@
-#include <vector>
-#include <unordered_map>
-using namespace std;
-
 class Solution {
 public:
+
     int minSubarray(vector<int>& nums, int p) {
-        long totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
-        }
-
-        // Find the remainder when total sum is divided by p
-        int rem = totalSum % p;
-        if (rem == 0) return 0; // If the remainder is 0, no subarray needs to be removed
-
-        unordered_map<int, int> prefixMod;
-        prefixMod[0] = -1;  // Initialize for handling full prefix
-        long prefixSum = 0;
-        int minLength = nums.size();
-
-        for (int i = 0; i < nums.size(); ++i) {
-            prefixSum += nums[i];
-            int currentMod = prefixSum % p;
-            int targetMod = (currentMod - rem + p) % p;
-
-            if (prefixMod.find(targetMod) != prefixMod.end()) {
-                minLength = min(minLength, i - prefixMod[targetMod]);
+        long long tot=0;
+        tot=accumulate(nums.begin(),nums.end(),0,[p](int sum ,int val){
+            return (val+sum)%p;
+        
+        });
+        if(tot%p==0)return 0;
+        long long tar=tot%p;
+        int ans=nums.size();
+        long long sum=0;
+        
+        unordered_map<int,int> map;
+        map[0]=-1;
+        for(int i=0;i<nums.size();i++){
+            int it=nums[i];
+            sum=(sum+it)%p;
+            
+            if(map.find((sum-tar+p)%p)!=map.end()){
+                ans=min(ans,(i-map[(sum-tar+p)%p]));
             }
-
-            prefixMod[currentMod] = i;
+            map[sum]=i;
         }
-
-        return minLength == nums.size() ? -1 : minLength;
+        
+        return nums.size()==ans?-1:ans;
     }
 };
