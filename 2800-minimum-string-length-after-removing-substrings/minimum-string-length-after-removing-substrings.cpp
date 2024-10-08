94a07fe58afc1256051c72d97a41f0c72c2cce64
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2024-10-08 05:33:22 +0530

    Time: 4 ms (75.53%) | Memory: 11.9 MB (15.34%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     int minLength(string s) {
-         stack<char> charStack;
-        for (char currentChar : s) {
-            if (!charStack.empty() && 
-                ((charStack.top() == 'A' && currentChar == 'B') || 
-                 (charStack.top() == 'C' && currentChar == 'D'))) {
-                charStack.pop(); // Remove the matching pair
-            } else {
-                charStack.push(currentChar); // Add character to stack
+        stack<int> st;
+        for(auto it:s){
+            if(!st.empty()){
+            if(it=='D'){
+                if(st.top()=='C'){
+                    st.pop();
+                   
+                }else{
+                    st.push(it);
+                }
+            }
+           else if(it=='B'){
+                if(st.top()=='A'){
+                    st.pop();
+                }else{
+                    st.push(it);
+                }
+            }
+            else{
+                st.push(it);
+            }
+        } else{
+                st.push(it);
             }
         }
-        return charStack.size(); 
+        return st.size();
     }
 };
