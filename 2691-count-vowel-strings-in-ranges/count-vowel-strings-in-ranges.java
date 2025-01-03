6dd40f79b2d843dba3f7bb3a5957260865f8fe6b
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2025-01-03 05:46:52 +0530

    Time: 18 ms (8.97%) | Memory: 85.5 MB (87.44%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
@@ -1,30 +1,32 @@
 class Solution {
-
     public int[] vowelStrings(String[] words, int[][] queries) {
-        int[] ans = new int[queries.length];
-        HashSet<Character> vowels = new HashSet<>(
-            Arrays.asList('a', 'e', 'i', 'o', 'u')
-        );
-        int[] prefixSum = new int[words.length];
-        int sum = 0;
-        for (int i = 0; i < words.length; i++) {
-            String currentWord = words[i];
-            if (
-                vowels.contains(currentWord.charAt(0)) &&
-                vowels.contains(currentWord.charAt(currentWord.length() - 1))
-            ) {
-                sum++;
+        HashSet<Character> set=new HashSet<>(Arrays.asList('a','e','i','o','u'));
+      
+
+        int [] ans=new int [queries.length];
+         int [] pre=new int[words.length];
+
+    if(set.contains(words[0].charAt(0)) && set.contains(words[0].charAt(words[0].length()-1)) ){
+        pre[0]=1;
+    }
+        for(int i=1;i<words.length;i++){
+            if(set.contains(words[i].charAt(0)) && set.contains(words[i].charAt(words[i].length()-1)) ){
+                pre[i]=pre[i-1]+1;
+            }else{
+                pre[i]=pre[i-1];
             }
-            prefixSum[i] = sum;
-        }
 
-        for (int i = 0; i < queries.length; i++) {
-            int[] currentQuery = queries[i];
-            ans[i] =
-                prefixSum[currentQuery[1]] -
-                (currentQuery[0] == 0 ? 0 : prefixSum[currentQuery[0] - 1]);
         }
-
+        int i=0;
+        for(int[] arr: queries){
+            if(arr[0]==0){
+                ans[i]=pre[arr[1]];
+            }else{
+                 ans[i]=pre[arr[1]]-pre[arr[0]-1];
+            }
+            i++;
+        }
+        System.out.println(Arrays.toString(pre));
         return ans;
     }
 }
