0a7e82f9729720e3e4eb179ee491a04b028cb258
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2025-01-03 09:00:04 +0530

    Time: 11 ms (20.63%) | Memory: 85.8 MB (63.68%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
@@ -6,27 +6,44 @@ class Solution {
         int [] ans=new int [queries.length];
          int [] pre=new int[words.length];
 
-    if(set.contains(words[0].charAt(0)) && set.contains(words[0].charAt(words[0].length()-1)) ){
-        pre[0]=1;
-    }
-        for(int i=1;i<words.length;i++){
-            if(set.contains(words[i].charAt(0)) && set.contains(words[i].charAt(words[i].length()-1)) ){
-                pre[i]=pre[i-1]+1;
-            }else{
-                pre[i]=pre[i-1];
-            }
+    pre[0]=(set.contains(words[0].charAt(0)) && set.contains(words[0].charAt(words[0].length()-1)) )? 1:0;
 
-        }
-        int i=0;
-        for(int[] arr: queries){
-            if(arr[0]==0){
-                ans[i]=pre[arr[1]];
-            }else{
-                 ans[i]=pre[arr[1]]-pre[arr[0]-1];
-            }
-            i++;
-        }
-        System.out.println(Arrays.toString(pre));
-        return ans;
+        IntStream.range(1,words.length)
+                 .forEach(i->{
+                    pre[i]=(set.contains(words[i].charAt(0)) && set.contains(words[i].charAt(words[i].length()-1)) ) ? pre[i-1]+1 :pre[i-1];
+                 });
+
+
+        // for(int i=1;i<words.length;i++){
+        //     if(set.contains(words[i].charAt(0)) && set.contains(words[i].charAt(words[i].length()-1)) ){
+        //         pre[i]=pre[i-1]+1;
+        //     }else{
+        //         pre[i]=pre[i-1];
+        //     }
+
+        // }
+        // int i=0;
+        // for(int[] arr: queries){
+        //     if(arr[0]==0){
+        //         ans[i]=pre[arr[1]];
+        //     }else{
+        //          ans[i]=pre[arr[1]]-pre[arr[0]-1];
+        //     }
+        //     i++;
+        // }
+       // System.out.println(Arrays.toString(pre));
+       // return ans;
+
+       return Arrays.stream(queries)
+       .mapToInt(
+        arr -> arr[0]==0 ? pre[arr[1]] : pre[arr[1]]-pre[arr[0]-1]
+        
+       ).toArray();
+
+    // return Arrays.stream(queries)
+    //          .mapToInt(query -> query[0] == 0
+    //                             ? pre[query[1]]
+    //                             : pre[query[1]] - pre[query[0] - 1])
+    //          .toArray();
     }
 }
