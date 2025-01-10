c5dabb29b19a673909171b8fe88749d90b728bad
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2025-01-10 21:35:11 +0530

    Time: 601 ms (5.02%) | Memory: 211.1 MB (5.02%) - LeetSync

--- 952-word-subsets/word-subsets.cpp
+++ 952-word-subsets/word-subsets.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
         vector<string> ans;
-      //  vector<unordered_map<char,int>> arr1(words1.size());
+     
           vector<unordered_map<char,int>> arr2(words2.size());
           unordered_map<char,int> required ;
-        
-            // 
-           
-
+     
           for(int i=0;i<words2.size();i++){
              string str=words2[i];
             for(char ch: str){
@@ -20,16 +17,12 @@ public:
            
             for(auto val:it){
                 required[val.first]=max(required[val.first],val.second);
-              //   cout<<val.first<<"-"<<val.second<<"  ";
+              
             }
-            cout<<endl;
+          
         }
            
-        // for(auto it:required){
-        //     cout<<it.first<<"-"<<it.second;
-        //     cout<<endl;
-        // }
-
+         
          
         for(int i=0;i<words1.size();i++){
 
@@ -37,9 +30,7 @@ public:
             for(auto ch:words1[i]){
                 map[ch]++;
             }
-            // for(auto it:map){
-            //     cout<<it.first<<" "<<it.second<<" -- " ;
-            // }
+            
             cout<<endl;
             bool flag=true;
 
