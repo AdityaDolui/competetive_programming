cc6ae8d709e34ad87af35fb22a2c6c4cfdf5abd5
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2025-01-29 13:20:55 +0530

    Time: 76 ms (5.12%) | Memory: 51.3 MB (25.31%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function(n) {
+    
+    console.log(n)
+   
+ return parseInt(n.toString(2).padStart(32,'0').split("").reverse().join(""),2  )
+   
+
+};
