35cdfca794bd1bba40eaa48ad0bad4eaa7cbe54e
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2025-03-15 06:10:39 +0530

    Time: 75 ms (5.32%) | Memory: 58 MB (99.87%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
@@ -1,24 +1,34 @@
 class Solution {
+    public boolean check(int ans,int[] candies, long k){
+       
+       int i=0,r=candies.length-1;
+       long count=0;
+       int rem=0;
+        
+        for(int val:candies){
+            count+=val/ans;
+            if(count>=k)return true;
+        }
+  
+    
+       if(count>=k)return true;
+       else return false;
+
+    }
     public int maximumCandies(int[] candies, long k) {
-        int left = 1, right = 10_000_000;
-        int result = 0;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            long childrenCount = 0;
-
-            for (int candy : candies) {
-                childrenCount += candy / mid;
-            }
-
-            if (childrenCount >= k) {
-                result = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+      
+        int ans=0;
+        System.out.println(Arrays.toString(candies));
+        int left=1,right=10000000;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(check(mid,candies,k)){
+                ans=mid;
+                left=mid+1;
+            }else{
+                    right=mid-1;
             }
         }
-
-        return result;
+return ans;
     }
 }
