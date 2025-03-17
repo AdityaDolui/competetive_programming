f047fdaae4949f32ae328dee597d4f509591e4d0
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2025-03-17 08:52:00 +0530

    Time: 15 ms (5.62%) | Memory: 45.3 MB (5.18%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
@@ -8,11 +8,5 @@ class Solution {
                     .values()
                     .stream()
                     .allMatch(val-> (val&1)!=1);
-
-
-        // Arrays.sort(nums);
-        // System.out.println(Arrays.toString(nums));
-        // if(res>0)return false;
-        // return true;
     }
 }
