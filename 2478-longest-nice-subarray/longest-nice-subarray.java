85f046c99bef17a2e6f38ddbc43ca4e4a373cd0c
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2025-03-18 08:46:16 +0530

    Time: 2 ms (100.00%) | Memory: 55.8 MB (57.77%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.java
+++ 2478-longest-nice-subarray/longest-nice-subarray.java
@@ -4,11 +4,9 @@ class Solution {
         int cal=nums[0];
         int l=0,h=1;
         while(h<nums.length){
-           // cal=cal&nums[h];
             if((cal & nums[h]) ==0){
                 ans=Math.max(ans,(h-l+1));
                 cal=cal|nums[h];
-                // System.out.println(l+" "+h+" ");
             }else{
             while(l<h){
                 if((cal & nums[h])==0)break;
@@ -16,8 +14,7 @@ class Solution {
                 l++;
             }
             cal=(cal | nums[h]);
-            }
-            
+            }   
             h++;
         }
         return ans;
