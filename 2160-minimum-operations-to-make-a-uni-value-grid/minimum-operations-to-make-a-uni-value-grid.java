caf21a0513707e2c6c952526b76eaa078778a2fb
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2025-03-27 05:02:15 +0530

    Time: 70 ms (55.28%) | Memory: 75.3 MB (54.27%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
@@ -2,18 +2,22 @@ class Solution {
     public int minOperations(int[][] grid, int x) {
         int[] newArr= Arrays.stream(grid)
                             .flatMapToInt(Arrays::stream)
+                            .sorted()
                             .toArray();
-        Arrays.sort(newArr);
+       
     int mid=newArr[(newArr.length/2)];
         
-        int cnt=0;
-        for(int val:newArr){
-            if(Math.abs(val-mid)%x==0)cnt+=(Math.abs(val-mid))/x;
-            else return -1;
-        }
+        return Arrays.stream(newArr)
+                        .map(val-> Math.abs(val-mid)%x==0?Math.abs(val-mid)/x:-1)
+                        .reduce(0,(a,b)-> b==-1?-1:(a==-1?-1:a+b));
+    //     int cnt=0;
+    //     for(int val:newArr){
+    //         if(Math.abs(val-mid)%x==0)cnt+=(Math.abs(val-mid))/x;
+    //         else return -1;
+    //     }
 
-    System.out.println(Arrays.toString(newArr));
-        return cnt;
+    // System.out.println(Arrays.toString(newArr));
+    //     return cnt;
        
     }
 }
