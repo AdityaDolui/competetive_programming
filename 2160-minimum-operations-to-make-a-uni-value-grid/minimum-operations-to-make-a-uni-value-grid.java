a8caa80f7bce38d3df5380dfa6f16774912588ee
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2025-03-27 05:03:49 +0530

    Time: 71 ms (55.28%) | Memory: 66.3 MB (91.96%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
@@ -10,14 +10,7 @@ class Solution {
         return Arrays.stream(newArr)
                         .map(val-> Math.abs(val-mid)%x==0?Math.abs(val-mid)/x:-1)
                         .reduce(0,(a,b)-> b==-1?-1:(a==-1?-1:a+b));
-    //     int cnt=0;
-    //     for(int val:newArr){
-    //         if(Math.abs(val-mid)%x==0)cnt+=(Math.abs(val-mid))/x;
-    //         else return -1;
-    //     }
-
-    // System.out.println(Arrays.toString(newArr));
-    //     return cnt;
+  
        
     }
 }
