e7619618154175581a56fc121a15b69356208ee2
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2025-04-09 17:28:43 +0530

    Time: 17 ms (6.10%) | Memory: 44.2 MB (95.93%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
@@ -2,9 +2,12 @@ class Solution {
     public int minOperations(int[] nums, int k) {
         Set<Integer> set=Arrays.stream(nums).boxed().collect(Collectors.toSet());
 
-        boolean check=set.stream().anyMatch(val-> val<k);
-        if(check==true)return -1;
-        return (int) set.stream().filter(val-> val!=k).count();
+        return set.stream()
+                    .anyMatch(val-> val<k)? -1
+                    : (int) set.stream().filter(val-> val!=k).count();
+
+       
+       
        
     }
 }
