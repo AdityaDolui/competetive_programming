764e7e2164deb5171a0ef8ae6412948644698bc6
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2025-04-18 04:56:33 +0530

    Time: 78 ms (5.38%) | Memory: 46.7 MB (6.74%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -10,15 +10,7 @@ class Solution {
                         .flatMap(entry->entry.getValue().stream())
                         .findFirst()
                         .orElse(-1);
-       
-        // s.chars()
-        //         .mapToObj(ch->(char)ch)
-        //         .collect(Collectors.groupingBy(Function.identity(),LinkedHashMap::new,Collectors.counting()))
-        //         .entrySet()
-        //         .stream()
-        //         .filter(entry->entry.getValue()==1)
-        //         .map(entry->entry.getKey())
-        //         .findFirst();
+        
         
     }
 }
