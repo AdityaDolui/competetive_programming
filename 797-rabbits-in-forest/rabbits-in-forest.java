6831d060a00f7f80998b6c0276713912ff1b9266
AdityaDolui <140312842+AdityaDolui@users.noreply.github.com>
2025-04-21 04:10:22 +0530

    Time: 9 ms (5.61%) | Memory: 43.1 MB (7.29%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.java
+++ 797-rabbits-in-forest/rabbits-in-forest.java
@@ -1,11 +1,10 @@
 class Solution {
     public int numRabbits(int[] answers) {
-        int sum= Arrays.stream(answers)
+       return Arrays.stream(answers)
                         .boxed()
                         .collect(Collectors.groupingBy(s->s,Collectors.counting()))
                         .entrySet()
                         .stream()
-                    
                         .map(s->{
                             int val=s.getKey()+1;
                             int div=(int)(s.getValue()/val);
@@ -14,15 +13,7 @@ class Solution {
                         })
                         .mapToInt(Integer::intValue)
                         .sum();
-    return sum;
-            // return sum + (int) Arrays.stream(answers)
-            //             .boxed()
-            //             .collect(Collectors.groupingBy(s->s, Collectors.counting()))
-            //             .entrySet()
-            //             .stream()
-            //             .filter(s-> s.getKey()==0)
-            //             .map(s->s.getValue())
-            //             .mapToLong(Long::longValue)
-            //             .sum();
+  
+          
     }
 }
